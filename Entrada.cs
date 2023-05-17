e081115b9d6635fe4c931d7e36c3c857b24b29a4
Leon <ltellechea@atilra.org.ar>
2023-05-17 16:11:39 -0300

    Agregado de campos fila y butaca a la clase Entrada

--- Entrada.cs
+++ Entrada.cs
@@ -11,18 +11,22 @@ namespace PNT1
         // Propiedades autoimplementadas
         public Pelicula pelicula { get; set; }
         public Sala sala { get; set; }
+        public int fila { get; set; }
+        public string butaca { get; set; }
 
         // Constructor parametrizado
-        public Entrada(Pelicula pelicula, Sala sala)
+        public Entrada(Pelicula pelicula, Sala sala, int fila, string butaca)
         {
             this.pelicula = pelicula;
             this.sala = sala;
+            this.fila = fila;
+            this.butaca = butaca;
         }
 
         // Override del método ToString para mostrar los datos de la entrada (llamados implicitos a ToString de Pelicula y Sala)
         public override string ToString()
         {
-            return $"Entrada para: {pelicula}\nEn sala: {sala}";
+            return $"Entrada para: {pelicula}\nSala: {sala}\nFila: {fila}\nButaca:{butaca}";
         }
     }
 }
